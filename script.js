2a2a4f1d64f15a1c550a1e7c0a2ddd5390f77515
Yogesh Patil <144556307+Yogagujar07@users.noreply.github.com>
2024-09-16 22:53:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function fibonacci(num) {
-// your code here
+    if (num === 0) return 0;
+    if (num === 1) return 1;
+
+    let a = 0; // F(0)
+    let b = 1; // F(1)
+    let c; // This will hold the Fibonacci number we are calculating
+
+    for (let i = 2; i <= num; i++) {
+        c = a + b; // Calculate the next Fibonacci number
+        a = b;     // Update a to the previous b
+        b = c;     // Update b to the newly calculated Fibonacci number
+    }
+
+    return b; // b is the Fibonacci number we want
 }
 
 module.exports = fibonacci;
+const fibonacci = require('./path/to/your/fibonacci');
+
+console.log(fibonacci(5)); // Output: 5
+console.log(fibonacci(10)); // Output: 55
